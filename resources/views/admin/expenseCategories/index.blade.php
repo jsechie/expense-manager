fd6e60a7f9690ef028da3ef3efef0dca00b939f7
JOSHUA SECHIE-OTSABAH <otsabahjoshua@gmail.com>
2021-12-05 18:04:39 +0000

    Petty Cash category can't be deleted added

--- resources/views/admin/expenseCategories/index.blade.php
+++ resources/views/admin/expenseCategories/index.blade.php
@@ -52,19 +52,21 @@
                                     </a>
                                 @endcan
 
-                                @can('expense_category_edit')
-                                    <a class="btn btn-xs btn-info" href="{{ route('admin.expense-categories.edit', $expenseCategory->id) }}">
-                                        {{ trans('global.edit') }}
-                                    </a>
-                                @endcan
+                                @if($expenseCategory->id !== 1)
+                                    @can('expense_category_edit')
+                                        <a class="btn btn-xs btn-info" href="{{ route('admin.expense-categories.edit', $expenseCategory->id) }}">
+                                            {{ trans('global.edit') }}
+                                        </a>
+                                    @endcan
 
-                                @can('expense_category_delete')
-                                    <form action="{{ route('admin.expense-categories.destroy', $expenseCategory->id) }}" method="POST" onsubmit="return confirm('{{ trans('global.areYouSure') }}');" style="display: inline-block;">
-                                        <input type="hidden" name="_method" value="DELETE">
-                                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                                        <input type="submit" class="btn btn-xs btn-danger" value="{{ trans('global.delete') }}">
-                                    </form>
-                                @endcan
+                                    @can('expense_category_delete')
+                                        <form action="{{ route('admin.expense-categories.destroy', $expenseCategory->id) }}" method="POST" onsubmit="return confirm('{{ trans('global.areYouSure') }}');" style="display: inline-block;">
+                                            <input type="hidden" name="_method" value="DELETE">
+                                            <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                                                <input type="submit" class="btn btn-xs btn-danger" value="{{ trans('global.delete') }}">
+                                            </form>
+                                    @endcan
+                                @endif
 
                             </td>
 
